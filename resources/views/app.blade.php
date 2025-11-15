54d83ff978d6290cd8fbd2e3281b9bd8b8979a1f
Naveen <naveenpunchihewa9@gmail.com>
2025-11-15 09:38:18 +0530

    Favicon-Applied

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,16 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon - Banana Theme -->
+        <link rel="icon" href="{{ asset('favicon.svg') }}?v={{ time() }}" type="image/svg+xml">
+        <link rel="icon" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext y='.9em' font-size='90'%3E🍌%3C/text%3E%3C/svg%3E" type="image/svg+xml">
+        <link rel="shortcut icon" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext y='.9em' font-size='90'%3E🍌%3C/text%3E%3C/svg%3E">
+        <link rel="apple-touch-icon" href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 180 180'%3E%3Crect width='180' height='180' fill='%23FCD34D'/%3E%3Ctext y='140' x='50' font-size='120'%3E🍌%3C/text%3E%3C/svg%3E">
+        
+        <!-- Meta tags for better icon support -->
+        <meta name="msapplication-TileColor" content="#FCD34D">
+        <meta name="theme-color" content="#FCD34D">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
